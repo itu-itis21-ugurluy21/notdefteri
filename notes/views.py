29124be17db0dbbbc7213d97d7432f6c22c81ffa
Yasin Uğurlu <yugurlu72@gmail.com>
2024-10-10 18:13:51 +0300

    Yasinden deneme

--- notes/views.py
+++ notes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def hello_world(request):
     return render(request, 'hello.html', {'message': 'Hello World'})
+
+def hello_world2(request):
+    return render(request, 'hello.html', {'message': 'Hello World'})
